02f6614d0f8da4232391785eb48f0e090d2bdf40
dotunakin <dotunprom@gmail.com>
2022-03-14 23:38:54 +0000

    condition

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+USER_ID=$(id -u)
+if [ "$USER_ID" -ne 0 ]; then
+  echo you should run your script as sudo or root user
+  exit
+fi
+
 echo -e "\e[36m Installing Nginx \e[0m"
 yum install nginx -y
 
