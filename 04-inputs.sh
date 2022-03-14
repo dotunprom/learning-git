4a848e605b184dada7a82046f77e2a2349bd7822
dotunakin <dotunprom@gmail.com>
2022-03-14 13:44:24 +0000

    variables declared, Dynamic data scenario with arrays

--- 04-inputs.sh
+++ 04-inputs.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-read -p "Enter your name: " name
+read 'Enter your name:' name
 echo "Your name = $name"
 
-# special variables
-# $0-$n, $* $#
-echo script Name = $0
-echo First Argument = $1
-echo All Arguments = $*
-echo Number of Arguments = $#
