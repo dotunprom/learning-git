1c473ffd5f54a905c945767267b3bde0ed2610cd
dotunakin <dotunprom@gmail.com>
2022-03-14 23:21:57 +0000

    condition

--- 07-condition.sh
+++ 07-condition.sh
@@ -11,6 +11,26 @@
 # commands
 # fi
 
+# ...IF ELSE...
+
+# if [ expression ]; then
+# commands
+# else
+# command
+# fi
+
+# ...ELIF...
+
+# if [ expression 1 ] ; then
+# commands
+# elif [ expression 2 ]; then
+# commands
+# elif [ expression 3]; then
+# commands
+# else
+# commands
+# fi
+
 # if [ 1 -eq 1 ]
 # then
 #   echo Hello
@@ -20,7 +40,10 @@
 # 1. String Tests
 # Operators : == , != , -z
 # 2. Number Tests
+# operators : -eq , -ne , -le , -lt , -gt
 # 3. File Tests
+# -e : use to check if file exist or not
+# https://tlpd.org/LDP/abs/html/fto.html
 
 a="abc"
 if [ "$a" == "abc" ]; then
@@ -33,6 +56,14 @@ fi
 
 if [ -z "$b" ]; then
   echo b variable is empty
+fi
+
+
+a="abc"
+if [ "$a" == "abc" ]; then
+  echo both are equal
+else
+  echo "both are not equal"
 fi
 
 # use quote for variables
