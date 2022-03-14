d4b3837b1d96d84ae5848c4d78880da56ba8d8c6
dotunakin <dotunprom@gmail.com>
2022-03-14 22:59:27 +0000

    condition

--- 07-condition.sh
+++ 07-condition.sh
@@ -11,7 +11,27 @@
 # commands
 # fi
 
-if [ 1 -eq 1 ]
-then
-   echo Hello
+# if [ 1 -eq 1 ]
+# then
+#   echo Hello
+# fi
+
+# Type of expression
+# 1. String Tests
+# Operators : == , != , -z
+# 2. Number Tests
+# 3. File Tests
+
+a="abc"
+if [ "$a" == "abc" ]; then
+  echo both are equal
 fi
+
+if [ "$a" != "abc" ]; then
+  echo "both are not equal"
+fi
+
+if [ -z "$b" ]; then
+  echo b variable is empty
+fi
+
