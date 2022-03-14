41e5f5278efa97eb6740171d7ef6f99f4fa64ef0
dotunakin <dotunprom@gmail.com>
2022-03-14 13:51:22 +0000

    Input special variables

--- 04-inputs.sh
+++ 04-inputs.sh
@@ -3,3 +3,10 @@
 read -p 'Enter your name:' name
 echo "Your name = $name"
 
+# special variables
+# $0-$n, $*/$@ , $#
+echo script Name = $0
+echo First Argument = $1
+echo All Arguments = $*
+echo Number of Arguments = $#
+
