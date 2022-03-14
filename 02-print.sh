006c439a008068e2fbe0920fe658c45b798a09ed
dotunakin <dotunprom@gmail.com>
2022-03-14 00:13:07 +0000

    simple script for echo print demo

--- 02-print.sh
+++ 02-print.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 
 echo Hello World.
+
+## Color codes
+# Red       31
+# Green     32
+# Yellow    33
+# Blue      34
+# Magenta   35
+# Cyan      36
+
+# Syntax : echo -e "\e[COLmHello\0M["
+# -e option is not enabled esc seq. without -e colors will not work
+# "" or '' Quote are mandatry for color to work.
+# \e[COLm : this to enable the color  COL is one of the color code.
+# \e[0m : disable the color
+
+echo -e "\e[31mText i red color"
+echo "One more line"
