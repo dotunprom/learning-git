0977ef8be5d4cd040bd9c77e568de695222e536a
dotunakin <dotunprom@gmail.com>
2022-03-15 12:50:57 +0000

    functions

---
+++ 08-functions.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+ # Variables : If we assing a name to set data that is available
+ # Function : If we assign a name to set of commands that is function
+
+#...declare a function...
+# func_name()
+# {
+# commands
+# commands
+# }
+
+#...access a fucntion...
+
+# func_name
+
+print _message()
+ { echo hello, echo good morning, welcome to devops}
+Print_Message
+
+#first declare a function, then call the function
+
+
