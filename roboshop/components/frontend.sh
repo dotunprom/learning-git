0708fd46f61af2d7983d212bbfddf9b4c37b251b
dotunakin <dotunprom@gmail.com>
2022-03-15 00:05:30 +0000

    condition

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -23,7 +23,7 @@ if [$? -eq 0 ]; then
   echo -e "\e[32mSUCCESS\e[0m"
 else
   echo -e "\e[31mFAILURE\e[0m"
-  exit 2
+  exit 1
 fi
 
 
@@ -39,7 +39,7 @@ if [$? -eq 0 ]; then
   echo -e "\e[32mSUCCESS\e[0m"
 else
   echo -e "\e[31mFAILURE\e[0m"
-  exit 2
+  exit
 fi
 
 echo -e "\e[36m Starting Nginx \e[0m"
