b06d432cb6966c09bd4aad24d24b8fc977df195e
dotunakin <dotunprom@gmail.com>
2022-03-15 13:40:11 +0000

    roboshop frontend setup

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+MyChoice(){
+
+  if [$1 -eq 0 ]; then
+  echo -e "\e[32mSUCCESS\e[0m"
+else
+  echo -e "\e[31mFAILURE\e[0m"
+  exit 2
+fi}
+
 USER_ID=$(id -u)
 if [ "$USER_ID" -ne 0 ]; then
   echo you should run your script as sudo or root user
@@ -8,23 +17,12 @@ fi
 
 echo -e "\e[36m Installing Nginx \e[0m"
 yum install nginx -y
+MyChoice $?
 
-
-if [$? -eq 0 ]; then
-  echo -e "\e[32mSUCCESS\e[0m"
-else
-  echo -e "\e[31mFAILURE\e[0m"
-  exit 2
-fi
 
 echo -e "\e[36m Downloading Nginx content\e[0m"
 curl -f -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
-if [$? -eq 0 ]; then
-  echo -e "\e[32mSUCCESS\e[0m"
-else
-  echo -e "\e[31mFAILURE\e[0m"
-  exit 2
-fi
+MyChoice $?
 
 
 echo -e "\e[36m Cleaning old Nginx content and extracting new download archive\e[0m"
@@ -35,13 +33,11 @@ mv frontend-main/* .
 mv static/* .
 rm -rf frontend-main README.md
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
-if [$? -eq 0 ]; then
-  echo -e "\e[32mSUCCESS\e[0m"
-else
-  echo -e "\e[31mFAILURE\e[0m"
-  exit 2
-fi
+MyChoice $?
+
 
 echo -e "\e[36m Starting Nginx \e[0m"
 systemctl restart nginx
+MyChoice $?
 systemctl enable nginx
+}
