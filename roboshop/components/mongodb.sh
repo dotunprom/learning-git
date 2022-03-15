9005e92a9881d2373b68c73296ffd9dd9ee0b09c
dotunakin <dotunprom@gmail.com>
2022-03-15 16:44:29 +0000

    Adding mongodb setup

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -27,15 +27,15 @@ print "Setup YUM Repos"
 curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo &>>$LOG_FILE
 MyChoice $?
 
-Print "Install Mongodb"
+Print "Install mongodb"
 yum install -y mongodb-org &>>$LOG_FILE
 MyChoice $?
 
-print "Update Mongodb listen Address"
-sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongodb.conf
+print "Update mongodb listen Address"
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 MyChoice $?
 
-Print "Start Mongodb"
+Print "Start mongodb"
 systemctl enable mongod &>>$LOG_FILE && systemctl start mongod &>>$LOG_FILE
 MyChoice $?
 
