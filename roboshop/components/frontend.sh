0c361480a9966e230bc85809be6146e92cdb2427
dotunakin <dotunprom@gmail.com>
2022-03-15 15:04:29 +0000

    roboshop frontend setup

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -19,6 +19,8 @@ if [ "$USER_ID" -ne 0 ]; then
   echo you should run your script as sudo or root user
   exit 1
 fi
+LOG_FILE=/tmp/roboshop.log
+rm -f $LOG_FILE
 
 Print "Installing Nginx"
 yum install nginx -y
@@ -26,27 +28,27 @@ MyChoice $?
 
 
 Print "Downloading Nginx content"
-curl -f -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
+curl -f -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" >>$LOG_FILE
 MyChoice $?
 
 
 Print "Cleaning old Nginx content"
-rm -rf /usr/share/nginx/html/*
+rm -rf /usr/share/nginx/html/* >>$LOG_FILE
 MyChoice $?
 
 cd /usr/share/nginx/html/
 
 Print "Extracting Archive"
-unzip /tmp/frontend.zip && mv frontend-main/* . && mv static/* .
+unzip /tmp/frontend.zip >>$LOG_FILE && mv frontend-main/* . >>$LOG_FILE && mv static/* . >>$LOG_FILE
 MyChoice $?
 
 # && : move to second line if firs command is good
 # || : if first is a failure, move the second command. if first is goo, it will go to second command.
 
 Print "Update roboshop configuration"
-mv localhost.conf /etc/nginx/default.d/roboshop.conf
+mv localhost.conf /etc/nginx/default.d/roboshop.conf >>$LOG_FILE
 MyChoice $?
 
 Print "Starting Nginx"
-systemctl restart nginx && systemctl enable nginx
+systemctl restart nginx >>$LOG_FILE && systemctl enable nginx >>$LOG_FILE
 MyChoice $?
