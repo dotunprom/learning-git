a8f8f6862223d36666a687a19e93d8bdfff4feaf
dotunakin <dotunprom@gmail.com>
2022-03-15 13:16:12 +0000

    functions

--- 08-functions.sh
+++ 08-functions.sh
@@ -24,6 +24,12 @@ Print_Message() {
   b=20
   }
 
+  STAT() {
+    echo hello
+    return 1
+    echo bye
+    }
+
 ## Main Program
 a=10
 Print_Message DevOps
@@ -31,11 +37,17 @@ Print_Message DevOps
 echo "First Argument in Main Script = $1"
 echo "Value of b = $b"
 
+STAT
+echo Exit status of function STAT = $?
+
+}
+
 
 
 
 # First declare a function, then call the function.
 # Function will have its own set of special variables
 # Variable declared in main program can be written in function and vice versa
+# Function is command and it has exit status as well.
 
 
