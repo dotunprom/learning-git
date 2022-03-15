caf67df5a9464969e0ea178534f33b78f8dc5f6e
dotunakin <dotunprom@gmail.com>
2022-03-15 13:49:12 +0000

    roboshop frontend setup

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -2,12 +2,13 @@
 
 MyChoice(){
 
-  if [$1 -eq 0 ]; then
+  if [ $1 -eq 0 ]; then
   echo -e "\e[32mSUCCESS\e[0m"
 else
   echo -e "\e[31mFAILURE\e[0m"
   exit 2
-fi}
+fi
+}
 
 USER_ID=$(id -u)
 if [ "$USER_ID" -ne 0 ]; then
