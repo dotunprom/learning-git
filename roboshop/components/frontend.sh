7229955e1c77478c9c31d65a94238b0eccba9ec6
dotunakin <dotunprom@gmail.com>
2022-03-15 14:00:22 +0000

    roboshop frontend setup

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -10,18 +10,22 @@ else
 fi
 }
 
+Print() {
+  echo -e "\e[36m $1 \e[0m"
+}
+
 USER_ID=$(id -u)
 if [ "$USER_ID" -ne 0 ]; then
   echo you should run your script as sudo or root user
   exit 1
 fi
 
-echo -e "\e[36m Installing Nginx \e[0m"
+Print "Installing Nginx"
 yum install nginx -y
 MyChoice $?
 
 
-echo -e "\e[36m Downloading Nginx content\e[0m"
+Print "Downloading Nginx content"
 curl -f -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
 MyChoice $?
 
@@ -36,7 +40,7 @@ rm -rf frontend-main README.md
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
 MyChoice $?
 
-echo -e "\e[36m Starting Nginx \e[0m"
+Print "Starting Nginx"
 systemctl restart nginx
 MyChoice $?
 systemctl enable nginx
