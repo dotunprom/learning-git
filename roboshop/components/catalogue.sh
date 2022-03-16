af0ddd1869aa21e5fd80964f317f00ac515ac4ae
dotunakin <dotunprom@gmail.com>
2022-03-16 21:53:20 +0000

    Adding mongodb setup

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -3,32 +3,32 @@
 source components/common.sh
 
 Print "Config YUM repo"
-curl -fsSL https://rpm.nodesource.com/setup_lts.x | bash - &>>$LOG_FILE
+curl -fsSL https://rpm.nodesource.com/setup_lts.x | bash - &>>${LOG_FILE}
 MyChoice $?
 
 Print "Install NodeJS"
-yum install nodejs gcc-c++ -y &>>$LOG_FILE
+yum install nodejs gcc-c++ -y &>>${LOG_FILE}
 MyChoice $?
 
 Print "Add User"
-useradd ${APP_USER} &>>$LOG_FILE
+useradd ${APP_USER} &>>${LOG_FILE}
 MyChoice $?
 
 Print "Download Application component"
-curl -f -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" &>>$LOG_FILE
+curl -f -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" &>>${LOG_FILE}
 MyChoice $?
 
 Print "Clean Old Content"
-rm -rf /home/${APP_USER}/catalogue &>>$LOG_FILE
+rm -rf /home/${APP_USER}/catalogue &>>${LOG_FILE}
 MyChoice $?
 
 
 Print "Extract App Content"
-cd /home/${APP_USER} &>>$LOG_FILE && unzip -o /tmp/catalogue.zip &>>$LOG_FILE && mv catalogue-main catalogue &>>$LOG_FILE
+cd /home/${APP_USER} &>>${LOG_FILE} && unzip -o /tmp/catalogue.zip &>>${LOG_FILE} && mv catalogue-main catalogue &>>${LOG_FILE}
 MyChoice $?
 
 Print "Install  App Dependencies"
-cd /home/${APP_USER}/catalogue &>>$LOG_FILE && npm install &>>$LOG_FILE
+cd /home/${APP_USER}/catalogue &>>${LOG_FILE} && npm install &>>${LOG_FILE}
 MyChoice $?
 
 # mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
