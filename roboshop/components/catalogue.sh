e2b5a9d88aa4bf63328f7d450bee6bedb4e7a497
dotunakin <dotunprom@gmail.com>
2022-03-17 13:43:18 +0000

    Adding mongodb setup

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -10,25 +10,13 @@ Print "Install NodeJS"
 yum install nodejs gcc-c++ -y &>>${LOG_FILE}
 MyChoice $?
 
-APP_SETUP() {
+Print "Add Application User"() {
   id ${APP_USER} &>>${LOG_FILE}
   if [ $? -ne 0 ]; then
     Print "Add Application User"
     useradd ${APP_USER} &>>${LOG_FILE}
     StatCheck $?
   fi
-  Print "Download App Component"
-  curl -f -s -L -o /tmp/${COMPONENT}.zip "https://github.com/roboshop-devops-project/${COMPONENT}/archive/main.zip" &>>${LOG_FILE}
-  StatCheck $?
-
-  Print "CleanUp Old Content"
-  rm -rf /home/${APP_USER}/${COMPONENT} &>>${LOG_FILE}
-  StatCheck $?
-
-  Print "Extract App Content"
-  cd /home/${APP_USER} &>>${LOG_FILE} && unzip -o /tmp/${COMPONENT}.zip &>>${LOG_FILE} && mv ${COMPONENT}-main ${COMPONENT} &>>${LOG_FILE}
-  StatCheck $?
-}
 
 
 Print "Download Application component"
