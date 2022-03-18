66339d2ccee1f4099082104b9c3f0c8fe536ce77
dotunakin <dotunprom@gmail.com>
2022-03-18 15:40:46 +0000

    Adding mongodb setup

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -3,36 +3,36 @@
 source components/common.sh
 
 Print "Config YUM repo"
-curl -fsSL https://rpm.nodesource.com/setup_lts.x | bash - &>>${LOG_FILE}
+curl -fsSL https://rpm.nodesource.com/setup_lts.x | bash - &>>$"{LOG_FILE}"
 MyChoice $?
 
 Print "Install NodeJS"
-yum install nodejs gcc-c++ -y &>>${LOG_FILE}
+yum install nodejs gcc-c++ -y &>>$"{LOG_FILE}"
 MyChoice $?
 
 Print "Add Application User"
-      id ${APP_USER} &>>${LOG_FILE}
+      id ${APP_USER} &>>$"{LOG_FILE}"
       if [ $? -ne 0 ]; then
          Print "Add Application User"
-         useradd ${APP_USER} &>>${LOG_FILE}
+         useradd ${APP_USER} &>>$"{LOG_FILE}"
          MyChoice $?
        fi
 
 Print "Download Application component"
-curl -f -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" &>>${LOG_FILE}
+curl -f -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" &>>$"{LOG_FILE}"
 MyChoice $?
 
 Print "Clean Old Content"
-rm -rf /home/${APP_USER}/catalogue &>>${LOG_FILE}
+rm -rf /home/${APP_USER}/catalogue &>>$"{LOG_FILE}"
 MyChoice $?
 
 
 Print "Extract App Content"
-cd /home/${APP_USER} &>>${LOG_FILE} && unzip -o /tmp/catalogue.zip &>>${LOG_FILE} && mv catalogue-main catalogue &>>${LOG_FILE}
+cd /home/${APP_USER} &>>$"{LOG_FILE}" && unzip -o /tmp/catalogue.zip &>>$"{LOG_FILE}" && mv catalogue-main catalogue &>>$"{LOG_FILE}"
 MyChoice $?
 
 Print "Install  App Dependencies"
-cd /home/${APP_USER}/catalogue &>>${LOG_FILE} && npm install &>>${LOG_FILE}
+cd /home/${APP_USER}/catalogue &>>$"{LOG_FILE}"&& npm install &>>$"{LOG_FILE}"
 MyChoice $?
 
 Print "Fix App_User Permissions"
@@ -41,11 +41,11 @@ MyChoice $?
 
 Print "Setup SystemD File"
 sed -i  -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/'
-/home/roboshop/${COMPONENT}/systemd.service &>>${LOG_FILE} && mv /home/roboshop/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service  &>>${LOG_FILE}
+/home/roboshop/${COMPONENT}/systemd.service &>>$"{LOG_FILE}" && mv /home/roboshop/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service  &>>$"{LOG_FILE}"
 MyChoice $?
 
 
 
 Print "Restart Catalogue Service"
-systemctl daemon-reload &>>${LOG_FILE} && systemctl restart catalogue &>>${LOG_FILE} && systemctl enable catalogue &>>${LOG_FILE}
+systemctl daemon-reload &>>$"{LOG_FILE}" && systemctl restart catalogue &>>$"{LOG_FILE}" && systemctl enable catalogue &>>$"{LOG_FILE}"
 MyChoice $?
