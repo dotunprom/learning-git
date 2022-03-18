e1ebd5a3b36f538335920b04731037f2316eac8a
dotunakin <dotunprom@gmail.com>
2022-03-18 23:47:10 +0000

    Adding mongodb setup

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -40,10 +40,15 @@ chown -R ${APP_USER}:${APP_USER} /home/${APP_USER}
 MyChoice $?
 
 Print "Setup SystemD File"
-sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/'
+ sed -i  -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/'
    /home/roboshop/catalogue/systemd.service &>>${LOG_FILE} && mv /home/roboshop/catalogue/systemd.service
    /etc/systemd/system/catalogue.service  &>>${LOG_FILE}
 MyChoice $?
+
+ #Print "Setup SystemD File"
+  #sed -i  -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' \
+          #/home/roboshop/catalogue/systemd.service &>>${LOG_FILE} && mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service  &>>${LOG_FILE}
+  #MyChoice $?
 
 
 Print "Restart Catalogue Service"
