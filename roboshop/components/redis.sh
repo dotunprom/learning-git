dc91c1de66ffd35b33a1609ec6f1ed75f54c6cb6
dotunakin <dotunprom@gmail.com>
2022-03-19 01:41:04 +0000

    Adding mongodb setup

---
+++ roboshop/components/redis.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+source components/common.sh
+
+Print "Setup YUM Repos"
+curl -L https://raw.githubusercontent.com/roboshop-devops-project/redis/main/redis.repo -o /etc/yum.repos.d/redis.repo &>>${LOG_FILE}
+MyChoice $?
+
+Print "Install Redis"
+yum install redis -y
+MyChoice $?
+
+Print "Update Redis Config"
+if
+  [ -f /etc/redis/redis.conf ]; then
+  sed -i -e 's/127.0.0.1/0.0.0.0' /etc/redis.conf
+fi
+MyChoice $?
+
+if
+  [ -f /etc/redis/redis.conf ]; then
+    sed -i -e 's/127.0.0.1/0.0.0.0' /etc/redis/redis.conf
+fi
+MyChoice $?
+
+Print "Start Redis Service"
+systemctl enable redis &>>${LOG_FILE} && systemctl start redis &>>${LOG_FILE}
+MyChoice $?
