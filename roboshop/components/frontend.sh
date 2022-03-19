40ecd9e94438f24f7866638d0959acf7fc6dd15a
dotunakin <dotunprom@gmail.com>
2022-03-19 00:42:06 +0000

    Adding mongodb setup

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -27,8 +27,7 @@ MyChoice $?
 
 Print "Update roboshop configuration"
 mv localhost.conf /etc/nginx/default.d/roboshop.conf &>>$LOG_FILE
-sed -i -e '/catalogue/s/localhost/catalogue.roboshop.internal/'
-/etc/nginx/default.d/roboshop.conf
+sed -i -e '/catalogue/s/localhost/catalogue.roboshop.internal/' /etc/nginx/default.d/roboshop.conf
 MyChoice $?
 
 Print "Starting Nginx"
