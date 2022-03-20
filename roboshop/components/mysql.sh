fa7401fd0a942ba26a912a44b952173ecf9105d4
dotunakin <dotunprom@gmail.com>
2022-03-20 09:24:08 +0000

    frontend fix

--- roboshop/components/mysql.sh
+++ roboshop/components/mysql.sh
@@ -16,6 +16,7 @@ systemctl enable mysqld &>>$LOG_FILE && systemctl start mysqld &>>$LOG_FILE
 MyChoice $?
 
 echo "SET PASSWORD FOR 'root'@localhost' = PASSWORD('Roboshop@1');" >/tmp/rootpass.sql
+
 DEFAULT_ROOT_PASSWORD=$(grep 'temporary password' /var/log/mysql.log | awk '{print $NF}')
 mysql -uroot -p"${DEFAULT_ROOT_PASSWORD}" </tmp/rootpass.sql
 
