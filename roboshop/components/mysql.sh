f5ea599047c42f5b2d63ab96d0efbfbf08f7c126
dotunakin <dotunprom@gmail.com>
2022-03-20 10:11:00 +0000

    frontend fix

--- roboshop/components/mysql.sh
+++ roboshop/components/mysql.sh
@@ -16,7 +16,6 @@ systemctl enable mysqld &>>$LOG_FILE && systemctl start mysqld &>>$LOG_FILE
 MyChoice $?
 
 echo 'show databases' | mysql -uroot -pRoboShop@1 &>>$LOG_FILE
- Print "Change Default Root Password"
  echo "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('Roboshop@1');" >/tmp/rootpass.sql
  DEFAULT_ROOT_PASSWORD=$(grep 'temporary password' /var/log/mysqld.log | awk '{print $NF}')
  mysql --connect-expired-password -uroot -p"${DEFAULT_ROOT_PASSWORD}" </tmp/rootpass.sql &>>$LOG_FILE
