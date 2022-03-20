16aa0842e92f4bfe56da52966e9ec1f63c30b702
dotunakin <dotunprom@gmail.com>
2022-03-20 08:55:12 +0000

    frontend fix

--- roboshop/components/mysql.sh
+++ roboshop/components/mysql.sh
@@ -3,7 +3,7 @@
 source components/common.sh
 
 
-Pring "configure YUM Repo"
+Print "configure YUM Repo"
 curl -f -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo &>>$LOG_FILE
 MyChoice $?
 
