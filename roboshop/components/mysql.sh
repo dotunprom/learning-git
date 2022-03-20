d7e0379f7741746f291518559bd0d904f7d7af46
dotunakin <dotunprom@gmail.com>
2022-03-20 10:29:04 +0000

    frontend fix

--- roboshop/components/mysql.sh
+++ roboshop/components/mysql.sh
@@ -15,9 +15,19 @@ Print "Start MySQL Service"
 systemctl enable mysqld &>>$LOG_FILE && systemctl start mysqld &>>$LOG_FILE
 MyChoice $?
 
-echo "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('RoboShop@1');" >/tmp/rootpass.sql
+#echo "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('RoboShop@1');" >/tmp/rootpass.sql
+#DEFAULT_ROOT_PASSWORD=$(grep 'temporary password' /var/log/mysqld.log | awk '{print $NF}')
+#mysql -uroot -p"${DEFAULT_ROOT_PASSWORD}" </tmp/rootpass.sql
+
+echo 'show databases' | mysql -uroot -pRoboShop@1 &>>$LOG_FILE
+if [ $? -ne 0 ]; then
+Print "Change Default Root Password"
+echo "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('Roboshop@1');" >/tmp/rootpass.sql
 DEFAULT_ROOT_PASSWORD=$(grep 'temporary password' /var/log/mysqld.log | awk '{print $NF}')
-mysql -uroot -p"${DEFAULT_ROOT_PASSWORD}" </tmp/rootpass.sql
+mysql --connect-expired-password -uroot -p"${DEFAULT_ROOT_PASSWORD}" </tmp/rootpass.sql &>>$LOG_FILE
+MyChoice $?
+fi
+
 
 
 ## mysql_secure_installation
