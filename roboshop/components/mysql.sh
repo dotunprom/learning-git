e82aacc2ec92a86166a4d0168ae2151ccbed33b3
dotunakin <dotunprom@gmail.com>
2022-03-21 17:25:09 +0000

    frontend fix

--- roboshop/components/mysql.sh
+++ roboshop/components/mysql.sh
@@ -17,7 +17,7 @@ MyChoice $?
 
 
 echo 'show databases' | mysql -uroot -pRoboShop@1 &>>${LOG_FILE}
-if [ $? -eq 0 ]; then
+if [ $? -ne 0 ]; then
 Print "Change Default Root Password"
 echo "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('Roboshop@1');" >/tmp/rootpass.sql &>>${LOG_FILE} DEFAULT_ROOT_PASSWORD=$(grep 'temporary password' /var/log/mysqld.log | awk '{print $NF}') mysql --connect-expired-password -uroot -p"${DEFAULT_ROOT_PASSWORD}" </tmp/rootpass.sql &>>${LOG_FILE}
 MyChoice $?
