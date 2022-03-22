27fd27dfb2295c29b0f4e78f5b612d3af23d6356
dotunakin <dotunprom@gmail.com>
2022-03-22 11:17:23 +0000

    Maven setup.

--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -66,6 +66,25 @@ SERVICE_SETUP(){
            MyChoice $?
 }
 
+NODEJS() {
+
+  Print "Configure Yum repos"
+  curl -fsSL https://rpm.nodesource.com/setup_lts.x | bash - &>>${LOG_FILE}
+  StatCheck $?
+
+  Print "Install NodeJS"
+  yum install nodejs gcc-c++ -y &>>${LOG_FILE}
+  StatCheck $?
+
+  APP_SETUP
+
+  Print "Install App Dependencies"
+  cd /home/${APP_USER}/${COMPONENT} &>>${LOG_FILE} && npm install &>>${LOG_FILE}
+  StatCheck $?
+
+  SERVICE_SETUP
+
+}
 
 MAVEN() {
   Print "Install Maven"
