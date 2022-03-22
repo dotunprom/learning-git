a39946828a64c5df5e1f40a0605b6e8505aeb618
dotunakin <dotunprom@gmail.com>
2022-03-22 11:27:40 +0000

    Maven setup.

--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -35,7 +35,7 @@ APP_SETUP(){
          curl -f -s -L -o /tmp/${COMPONENT}.zip "https://github.com/roboshop-devops-project/${COMPONENT}/archive/main.zip" &>>${LOG_FILE}
          MyChoice $?
 
-         Print "Clean Old Content"
+         Print "CleanUp Old Content"
          rm -rf /home/${APP_USER}/${COMPONENT} &>>${LOG_FILE}
          MyChoice $?
 
