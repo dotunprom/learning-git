817814f141744d7b0d878652a7aa5d176eaf3ae3
dotunakin <dotunprom@gmail.com>
2022-03-22 11:26:32 +0000

    Maven setup.

--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -14,7 +14,7 @@ Print() {
 }
 USER_ID=$(id -u)
 if [ "$USER_ID" -ne 0 ]; then
-  echo you should run your script as sudo or root user
+  echo You should run your script as sudo or root user
   exit 1
 fi
 LOG_FILE=/tmp/roboshop.log
