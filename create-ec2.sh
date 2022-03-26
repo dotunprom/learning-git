c67fa3a059d01426d2462ae336a3601dc7d8660c
dotunakin <dotunprom@gmail.com>
2022-03-26 08:20:46 +0000

    IP

--- create-ec2.sh
+++ create-ec2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-AMI_ID=$(aws ec2 describe-images --filters "Name=name, Values=Centos-7-Devops-Practice" | jq '.images[].ImageID' | sed -e 's/"//g')
+AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-Devops-Practice" | jq '.images[].ImageID' | sed -e 's/"//g')
 
 echo $AMI_ID
-aws ec2 run-instances --image-id ${AMI_ID} --instance-type t2.micro
+aws ec2 run-instances --image-id${AMI_ID} --instance-type t2.micro
