ecf8894a12e75662c1f61b75b8bc4475ec580031
dotunakin <dotunprom@gmail.com>
2022-03-26 08:25:31 +0000

    IP

--- create-ec2.sh
+++ create-ec2.sh
@@ -3,4 +3,4 @@
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-Devops-Practice" | jq '.images[].ImageID' | sed -e 's/"//g')
 
 echo $AMI_ID
-aws ec2 run-instances --image-id ${AMI_ID} --instance-type t2.micro
+aws ec2 run-instances --image-id${AMI_ID} --instance-type t2.micro
