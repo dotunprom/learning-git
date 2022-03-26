08442bda529f307b76498e21823dd626dfe9a595
dotunakin <dotunprom@gmail.com>
2022-03-26 08:59:10 +0000

    Create Instance.

--- create-ec2.sh
+++ create-ec2.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+if [ -z "$1" ]; then
+  echo -e "\e[31mInput Machine Name is needed\e[0m"
+  exit
+fi
+
+COMPONENT=$1
+
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice" | jq '.Images[].ImageId' | sed -e 's/"//g')
 
 echo $AMI_ID
